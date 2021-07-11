d854bb0d8f19d5737f26122472c66b7275106eb5
canesche <michael.canesche@gmail.com>
2021-07-11 19:24:39 -0300

    update

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -20,9 +20,8 @@ import tensorflow as tf
 import keras
 import keras.backend as K
 import keras.layers as KL
-#import keras.engine as KE
+import keras.engine as KE
 import keras.models as KM
-import keras.engine.topology as KE
 
 from mrcnn import utils
 
